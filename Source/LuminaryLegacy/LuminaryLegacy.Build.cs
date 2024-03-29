dfc3270be786b2d7b75b80e61cce3c9c841f2835
Igor Bulakh <igorbulakh1987@yandex.ru>
2024-03-29 15:36:23 +0300

    Add PublicIncludePath to LubminaryLegacy.Build.cs

--- Source/LuminaryLegacy/LuminaryLegacy.Build.cs
+++ Source/LuminaryLegacy/LuminaryLegacy.Build.cs
@@ -11,6 +11,8 @@ public class LuminaryLegacy : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		PublicIncludePaths.AddRange(new string[] { "LuminaryLegacy/Public/Player" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
